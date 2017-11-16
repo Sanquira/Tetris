0e8f5070d0dde7e7a995ad87669720e00ab3baea
Sanquira Van Delbar <sanquira.van.delbar@gmail.com>
2017-11-16 15:46:36 +0100

    Tetris 2D initial commit

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,145 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+
+    public int states = 4;
+
+    private int rotation = 0;
+    private GameController gameController;
+    private float fallTimer;
+
+    void Start()
+    {
+        GameObject gameControllerObject = GameObject.FindGameObjectWithTag("GameController");
+        if (gameControllerObject != null)
+        {
+            gameController = gameControllerObject.GetComponent<GameController>();
+        }
+        if (gameController == null)
+        {
+            Debug.Log("Cannot find 'GameController' script");
+        }
+    }
+
+    public void Rotate()
+    {
+        if (states == 1)
+            return;
+
+        rotation++;
+
+        if (states == 2)
+        {
+            int dir = ((rotation % 2) * 2) - 1;
+            RotateChildrens(-dir);
+            RotateParent(dir);
+            return;
+        }
+        if (states == 4)
+        {
+            RotateChildrens(-1);
+            RotateParent(1);
+            return;
+        }
+
+        Debug.Log("Wrong number of states.");
+    }
+
+    private void RotateBack()
+    {
+        if (states == 1)
+            return;
+
+        rotation--;
+
+        if (states == 2)
+        {
+            int dir = ((rotation % 2) * 2) - 1;
+            RotateChildrens(-dir);
+            RotateParent(dir);
+            return;
+        }
+        if (states == 4)
+        {
+            RotateChildrens(1);
+            RotateParent(-1);
+            return;
+        }
+
+        Debug.Log("Wrong number of states.");
+    }
+
+    private void RotateChildrens(int direction)
+    {
+        Transform[] childTransform = GetComponentsInChildren<Transform>();
+        for (int i = 0; i < childTransform.Length; i++)
+        {
+            if (childTransform[i] == transform)
+                continue;
+            childTransform[i].Rotate(0, 0, direction * -90, Space.Self);
+        }
+    }
+
+    private void RotateParent(int direction)
+    {
+        transform.Rotate(0, 0, direction * -90, Space.Self);
+    }
+
+    void Update()
+    {
+        if (gameController.IsPaused || !this.enabled)
+            return;
+
+        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+        {
+            transform.position += Vector3.right;
+            if (!gameController.IsInsideGrid(gameObject) || gameController.IsOverlapringAnotherBlock(gameObject))
+            {
+                transform.position -= Vector3.right;
+            }
+        }
+
+        else if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+        {
+            transform.position += Vector3.left;
+            if (!gameController.IsInsideGrid(gameObject) || gameController.IsOverlapringAnotherBlock(gameObject))
+            {
+                transform.position -= Vector3.left;
+            }
+        }
+
+        else if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
+        {
+            Rotate();
+            if (!gameController.IsInsideGrid(gameObject) || gameController.IsOverlapringAnotherBlock(gameObject))
+            {
+                RotateBack();
+            }
+        }
+
+        else if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+        {
+            transform.position += Vector3.down;
+            if (!gameController.IsInsideGrid(gameObject) || gameController.IsOverlapringAnotherBlock(gameObject))
+            {
+                transform.position -= Vector3.down;
+            }
+        }
+
+        fallTimer += Time.deltaTime;
+        if (fallTimer >= gameController.FallSpeed)
+        {
+            transform.position += Vector3.down;
+            if (!gameController.IsInsideGrid(gameObject) || gameController.IsOverlapringAnotherBlock(gameObject))
+            {
+                this.enabled = false;
+                transform.position -= Vector3.down;
+                gameController.OnBlockFalled(gameObject);
+            }
+            fallTimer = 0;
+        }
+    }
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,240 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public Vector2 arraySize = new Vector2(10, 22);
+    public Transform spawnPosition;
+    public Transform nextBlockPreviewPosition;
+    public TextMesh scoreValueText;
+    public TextMesh rowsValueText;
+    public GameObject gameOverText;
+    public GameObject pausedText;
+    public GameObject[] blockList;
+    public float speedupCoeficient = 4;
+
+    public GameObject debugQuad;
+
+    public float FallSpeed { get { return fallSpeed; } } private float fallSpeed;
+    public bool IsPaused { get { return isPaused; } } private bool isPaused;
+
+    private GameObject nextBlock;
+    private int nextBlockID;
+    private int score;
+    private int rows;
+    private GameObject[,] playGrid;
+    private bool gameOver;
+    private Bounds positionBoundaries;
+
+    void Start()
+    {
+        score = 0;
+        rows = 0;
+        scoreValueText.text = score + "";
+        rowsValueText.text = rows + "";
+        positionBoundaries = new Bounds(new Vector3(spawnPosition.transform.position.x + 0.5f, spawnPosition.transform.position.y - arraySize.x - 0.5f), new Vector3(arraySize.x, arraySize.y, 1));
+        playGrid = new GameObject[(int)arraySize.x, (int)arraySize.y];
+        gameOver = false;
+        isPaused = false;
+        gameOverText.SetActive(false);
+        pausedText.SetActive(isPaused);
+        UpdateFallSpeed();
+        Spawn(Random.Range(0, blockList.Length));
+        GenerateAndShowNextBlock();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P) && !gameOver)
+        {
+            isPaused = !isPaused;
+            pausedText.SetActive(isPaused);
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        if (Debug.isDebugBuild)
+        {
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawWireCube(positionBoundaries.center, positionBoundaries.size);
+        }
+    }
+
+    public void OnBlockFalled(GameObject block)
+    {
+        PlaceBlockToGrid(block);
+        int deletedRows = CheckFullRows();
+        if (deletedRows > 0)
+        {
+            rows += deletedRows;
+            score += GetScoreFromRows(deletedRows);
+            scoreValueText.text = score + "";
+            rowsValueText.text = rows + "";
+            UpdateFallSpeed();
+            CleanUpObjects();
+        }
+        if (!gameOver)
+        {
+            Spawn(nextBlockID);
+            GenerateAndShowNextBlock();
+        }
+    }
+
+    public bool IsInsideGrid(GameObject block)
+    {
+        Transform[] childTransforms = block.GetComponentsInChildren<Transform>();
+        for (int i = 0; i < childTransforms.Length; i++)
+        {
+            if (childTransforms[i] == block.transform)
+                continue;
+            if (!positionBoundaries.Contains(childTransforms[i].position))
+                return false;
+        }
+        return true;
+    }
+
+    public bool IsOverlapringAnotherBlock(GameObject block)
+    {
+        Transform[] childTransforms = block.GetComponentsInChildren<Transform>();
+
+        for (int j = 0; j < childTransforms.Length; j++)
+        {
+            if (childTransforms[j] == block.transform)
+                continue;
+
+            Vector3 position = spawnPosition.transform.position - childTransforms[j].position;
+            position.x += (arraySize.x / 2f);
+
+            if (playGrid[Mathf.RoundToInt(position.x), Mathf.RoundToInt(position.y)] != null)
+                return true;
+        }
+        return false;
+    }
+
+    public void UpdateFallSpeed()
+    {
+        fallSpeed = Mathf.Pow(speedupCoeficient, -(score / 50000f));
+    }
+
+    private void Spawn(int idx)
+    {
+        if (gameOver)
+        {
+            Debug.Log("Trying to spawn block even if game is over");
+            return;
+        }
+        GameObject block = Instantiate(blockList[idx], transform);
+        block.transform.SetPositionAndRotation(spawnPosition.position, Quaternion.identity);
+        for (int i = 0; i < arraySize.y && !IsInsideGrid(block); i++)
+        {
+            block.transform.position += Vector3.down; //move down
+        }
+        if (!IsInsideGrid(block))
+        {
+            Debug.Log("Cannot fit inside grid.");
+            Destroy(block);
+            return;
+        }
+        if (IsOverlapringAnotherBlock(block))
+        {
+            Destroy(block);
+            GameOver();
+            return;
+        }
+    }
+
+    private void GameOver()
+    {
+        gameOver = true;
+        gameOverText.SetActive(true);
+    }
+
+    private void CleanUpObjects()
+    {
+        GameObject[] objects = GameObject.FindGameObjectsWithTag("Block");
+        for (int i = 0; i < objects.Length; i++)
+        {
+            if (objects[i].GetComponentsInChildren<Transform>().Length <= 1)
+                Destroy(objects[i]);
+        }
+    }
+
+    private void GenerateAndShowNextBlock()
+    {
+        Destroy(nextBlock);
+        nextBlockID = Random.Range(0, blockList.Length);
+        nextBlock = (GameObject)Instantiate(blockList[nextBlockID], nextBlockPreviewPosition);
+        Bounds B = nextBlock.GetComponentsInChildren<Renderer>().Select(r => r.bounds).Aggregate((b1, b2) => { b1.Encapsulate(b2); return b1; });
+        nextBlock.transform.position += nextBlock.transform.position - B.center;
+        nextBlock.GetComponent<Block>().enabled = false;
+    }
+
+    private int GetScoreFromRows(int rows)
+    {
+        return (int)((Mathf.Pow(2, rows) - 1) * 100);
+    }
+
+    private void PlaceBlockToGrid(GameObject block)
+    {
+        Transform[] childTransforms = block.GetComponentsInChildren<Transform>();
+
+        for (int i = 0; i < childTransforms.Length; i++)
+        {
+            if (childTransforms[i] == block.transform)
+                continue;
+            Vector3 position = spawnPosition.transform.position - childTransforms[i].position;
+            position.x += (arraySize.x / 2f);
+
+            playGrid[Mathf.RoundToInt(position.x), Mathf.RoundToInt(position.y)] = childTransforms[i].gameObject;
+
+        }
+    }
+
+    private int CheckFullRows()
+    {
+        int rowsDeleted = 0;
+        for (int i = playGrid.GetLength(1) - 1; i >= 0; i--)
+        {
+            int sum = 0;
+            for (int j = 0; j < playGrid.GetLength(0); j++)
+            {
+                sum += playGrid[j, i] == null ? 0 : 1;
+            }
+            if (sum == playGrid.GetLength(0))
+            {
+                rowsDeleted++;
+                DeleteRow(i);
+                i++;
+            }
+        }
+        return rowsDeleted;
+    }
+
+    private void DeleteRow(int removeIdx)
+    {
+        for (int j = 0; j < playGrid.GetLength(0); j++)
+        {
+            Destroy(playGrid[j, removeIdx]);
+            playGrid[j, removeIdx] = null;
+            for (int l = removeIdx - 1; l >= 0; l--)
+            {
+                if (playGrid[j, l] != null)
+                {
+                    playGrid[j, l].transform.position += Vector3.down;
+                    playGrid[j, l + 1] = playGrid[j, l];
+                    playGrid[j, l] = null;
+                }
+            }
+        }
+    }
+
+}
